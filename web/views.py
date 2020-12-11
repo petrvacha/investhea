0b86769bb2a0c5b0882ac94bb818755dab02eaf0
Petr Vacha <pan.vacha@gmail.com>
2020-12-11 23:29:08 +0100

    added login, logout, registration, reset password

--- web/views.py
+++ web/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from web.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from web.forms import SignUpForm
+from web.tokens import account_activation_token
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+from investhea.settings import EMAIL_HOST_USER
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your Investhea Account'
+            message = render_to_string('register/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message, EMAIL_HOST_USER, fail_silently= False)
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, "register/register.html", {
+        'form': form
+    })
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return render(request, 'register/account_activation_invalid.html')
+
+
+def account_activation_sent(request):
+    return render(request, "register/account_activation_sent.html")
+
+
+@login_required
+def dashboard():
+    return render("dashboard/dashboard.html", {})
